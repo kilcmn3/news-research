52f21cfaef6f0883daa28242142eecb3a25052dc
kilcmn <kilcmn3@hotmail.com>
2021-02-03 01:08:29 -0500

    changed MainContainer name to DisplayContainer..

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+import DisplayContainer from './DisplayContainer';
+
+function App() {
+  return (
+    <div className='App'>
+      App
+      <DisplayContainer />
+    </div>
+  );
+}
+
+export default App;
