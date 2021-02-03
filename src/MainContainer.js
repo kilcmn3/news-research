e09dd04dd7e24670aaac1cc1fe6b5202e23c4133
kilcmn <kilcmn3@hotmail.com>
2021-02-03 01:01:53 -0500

    fetching api topstroies confirmed receiving..

--- src/MainContainer.js
+++ src/MainContainer.js
@@ -4,18 +4,18 @@ class MainContainer extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      listNews: [],
+      topstories: [],
     };
   }
 
   componentDidMount() {
     fetch('https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty')
       .then((response) => response.json())
-      .then((list) => console.log(list));
+      .then((data) => this.setState({ topstories: data }));
   }
 
   render() {
-    return <div className='MainContainer'></div>;
+    return <div className='MainContainer'>Main Container</div>;
   }
 }
 
