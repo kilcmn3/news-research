22bd79a252101f4767cd48db0e264c8268e58885
kilcmn <kilcmn3@hotmail.com>
2021-02-03 05:54:25 -0500

    prettier not working on jsx  extension..

---
+++ src/DisplayCards.jsx
@@ -0,0 +1,16 @@
+import React, {Component} from 'react'
+
+class DisplayCards extends Component {
+    constructor(props){
+        super(props)
+        this.state ={
+            article: {}
+        }
+    }
+
+    componentDidMount(){
+
+    }
+}
+
+export default DisplayCards
--- src/DisplayContainer.jsx
+++ src/DisplayContainer.jsx
@@ -1,21 +1,27 @@
 import React, { Component } from 'react';
-
+import DisplayCards from './DisplayCards'
 class DisplayContainer extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      topstories: [],
+      topStories: [],
     };
   }
 
   componentDidMount() {
     fetch('https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty')
       .then((response) => response.json())
-      .then((data) => this.setState({ topstories: data }));
+      .then((data) => this.setState({ topStories: data }));
+  }
+
+   scanItems() {
+    return 
   }
 
   render() {
-    return <div className='DisplayContainer'>Main Container</div>;
+    return <div className='DisplayContainer'>Main Container
+      <DisplayCards />
+    </div>;
   }
 }
 
