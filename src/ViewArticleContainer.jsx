56a88d4a6d232747eaa420823dfa6df5534070ba
kilcmn <kilcmn3@hotmail.com>
2021-02-12 15:46:43 -0500

    working on viewArticleContainer for fetching comments...

--- src/ViewArticleContainer.jsx
+++ src/ViewArticleContainer.jsx
@@ -1,20 +1,42 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 
+
 const ViewArticleContainer = (props) => {
+  
   const [comments, setComments] = useState([]);
   let params = useParams();
+  const _newsAPI = `https://hacker-news.firebaseio.com/v0/item/${params.itemId}.json?print=pretty`
 
   useEffect(() => {
-    fetch(
-      `https://hacker-news.firebaseio.com/v0/item/${params.itemId}.json?print=pretty`
+    fetch(_newsAPI
+      
     )
       .then((response) => response.json())
       .then((data) => setComments(data));
   }, []);
 
-  console.log(comments);
-  return <div>Hello this is View ViewArticleContainer</div>;
+  return (<center>
+    <table id="hnmain">
+      <tbody>
+        <tr>
+          <td>
+            <table class="fatitem">
+              <tbody>
+                <tr class="athing" id={params.itemId}>
+                  <td className="title">
+                  <a href= {_newsAPI}></a>
+                  </td>
+                </tr>
+              </tbody>
+              <br/>
+              <br/>
+            </table>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </center>;)
 };
 
 export default ViewArticleContainer;
