7a9fe799bef150d99bdafc3c59241501aa550c47
kilcmn <kilcmn3@hotmail.com>
2021-02-16 21:54:29 -0500

    asynchronus fetching api;rendering after recevied data from api

--- src/DisplayCards.jsx
+++ src/DisplayCards.jsx
@@ -10,58 +10,76 @@ import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 
 const DisplayCards = (props) => {
-  const [story, setStory] = useState([]);
+  const [story, setStory] = useState({});
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  async function dataAPI() {
+    try {
+      await fetch(
+        `https://hacker-news.firebaseio.com/v0/item/${props.item}.json?print=pretty`
+      )
+        .then((response) => response.json())
+        .then((data) => setStory(data));
+      await setLoading(true);
+    } catch (e) {
+      setError(e);
+    }
+  }
 
   useEffect(() => {
-    fetch(
-      `https://hacker-news.firebaseio.com/v0/item/${props.item}.json?print=pretty`
-    )
-      .then((response) => response.json())
-      .then((data) => setStory(data));
-
-    return () => setStory([]);
+    // fetch(
+    //   `https://hacker-news.firebaseio.com/v0/item/${props.item}.json?print=pretty`
+    // )
+    //   .then((response) => response.json())
+    //   .then((data) => setStory(data));
+    dataAPI();
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   return (
     <article className='Story'>
-      <div className='Story_container'>
-        <div className='Story_data'>
-          <div className='Story_title'>
-            <Link
-              to={{
-                pathname: `/item/${story.id}`,
-              }}>
-              {story.title}
-            </Link>
-            <a href={story.url}>({story.url})</a>
-          </div>
-          <div className='Story_meta'>
-            <span>
-              <a href={story.url}>{story.score} points</a>
-            </span>
-            <span className='Story_separator'>|</span>
-            <span>
-              <a href={story.url}>{story.by}</a>
-            </span>
-            <span className='Story_separator'>|</span>
-            <span>
-              <a href={story.url}>{story.time}</a>
-            </span>
-            <span className='Story_separator'>|</span>
-            <span>
-              <a href={story.url}>{story.time} ago</a>
-            </span>
-            <span className='Story_separator'>|</span>
-            <span>
-              <a href={story.url}>
-                {story.kids !== undefined ? story.kids.length : 0}
-                times
-              </a>
-            </span>
+      {loading ? (
+        <div className='Story_container'>
+          <div className='Story_data'>
+            <div className='Story_title'>
+              <Link
+                to={{
+                  pathname: `/item/${story.id}`,
+                }}>
+                {story.title}
+              </Link>
+              <a href={story.url}>({story.url})</a>
+            </div>
+            <div className='Story_meta'>
+              <span>
+                <a href={story.url}>{story.score} points</a>
+              </span>
+              <span className='Story_separator'>|</span>
+              <span>
+                <a href={story.url}>{story.by}</a>
+              </span>
+              <span className='Story_separator'>|</span>
+              <span>
+                <a href={story.url}>{story.time}</a>
+              </span>
+              <span className='Story_separator'>|</span>
+              <span>
+                <a href={story.url}>{story.time} ago</a>
+              </span>
+              <span className='Story_separator'>|</span>
+              <span>
+                <a href={story.url}>
+                  {story.kids !== undefined ? story.kids.length : 0}
+                  times
+                </a>
+              </span>
+            </div>
           </div>
         </div>
-      </div>
+      ) : (
+        'loading'
+      )}
     </article>
   );
 };
