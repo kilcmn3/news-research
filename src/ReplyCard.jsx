fc049abd255812417a31ecc3ee589aba297ce1cc
kilcmn <kilcmn3@hotmail.com>
2021-02-18 22:30:30 -0500

    created ReplyCard componenet

---
+++ src/ReplyCard.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const ReplyCard = (props) => {
+  console.log(props);
+  return <div></div>;
+};
+
+export default ReplyCard;
--- src/ViewArticleCard.jsx
+++ src/ViewArticleCard.jsx
@@ -5,6 +5,7 @@
 
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
+import ReplyCard from './ReplyCard';
 
 const ViewArticleCard = (props) => {
   const [comment, setComment] = useState(null);
@@ -21,6 +22,12 @@ const ViewArticleCard = (props) => {
 
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
+
+  const commentReplies = () => {
+    return comment.kids.map((kid, index) => {
+      return <ReplyCard key={index} kid={kid} />;
+    });
+  };
   return (
     <tbody>
       {comment !== null ? (
@@ -39,7 +46,13 @@ const ViewArticleCard = (props) => {
                           }}>
                           {comment.by}
                         </Link>
+                        <span className='age'>{comment.time}</span>
                       </span>
+                    </div>
+                    <br></br>
+                    <div className='comment'>
+                      <span className='commtext c00'>{comment.text}</span>
+                      <div className='reply'></div>
                     </div>
                   </td>
                 </tr>
