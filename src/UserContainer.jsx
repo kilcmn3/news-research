f622a8eae6e3667f5be2c762142ab7a132874905
kilcmn <kilcmn3@hotmail.com>
2021-02-22 00:05:36 -0500

    sucessfully fetch user info

--- src/UserContainer.jsx
+++ src/UserContainer.jsx
@@ -3,16 +3,39 @@
  *-[]Imply style on <div> tag
  **/
 
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 
 const UserContainer = (props) => {
+  const [user, setUser] = useState('');
+
   let params = useParams();
 
+  useEffect(() => {
+    fetch(
+      ` https://hacker-news.firebaseio.com/v0/user/${params.userName}.json?print=pretty`
+    )
+      .then((response) => response.json())
+      .then((data) => setUser(data));
+  }, []);
+
+  console.log(user);
   return (
     <center>
       {/* style = style="margin-top:2px; margin-bottom:-10px;" */}
-      <table id='hnmain'></table>
+      <table id='hnmain'>
+        <tbody>
+          <tr>
+            <td>
+              <table border='0'>
+                <tbody>
+                  <tr className='athing'></tr>
+                </tbody>
+              </table>
+            </td>
+          </tr>
+        </tbody>
+      </table>
     </center>
   );
 };
