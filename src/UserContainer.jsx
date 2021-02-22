f05fd5aa4e4021c7d93645b69100990025d29800
kilcmn <kilcmn3@hotmail.com>
2021-02-21 23:59:40 -0500

    styling table on UserContainer

--- src/UserContainer.jsx
+++ src/UserContainer.jsx
@@ -1,10 +1,20 @@
+/**
+ *TODO:
+ *-[]Imply style on <div> tag
+ **/
+
 import React from 'react';
 import { useParams } from 'react-router-dom';
 
 const UserContainer = (props) => {
   let params = useParams();
 
-  return <div> </div>;
+  return (
+    <center>
+      {/* style = style="margin-top:2px; margin-bottom:-10px;" */}
+      <table id='hnmain'></table>
+    </center>
+  );
 };
 
 export default UserContainer;
