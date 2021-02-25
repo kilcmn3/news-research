62fad45e310ffeffaca29effc626b2b543cd3ef2
kilcmn <kilcmn3@hotmail.com>
2021-02-24 23:26:28 -0500

    eslint disable ..

--- src/ViewArticleContainer.jsx
+++ src/ViewArticleContainer.jsx
@@ -38,6 +38,7 @@ const ViewArticleContainer = (props) => {
     // eslint-disable-next-line react-hooks/exhaustive-deps
 
     return () => setArticle(null);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   const scanComments = () => {
