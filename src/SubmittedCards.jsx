a9bfb86de65d2df58f42cfd4b46f6955c793f3fe
kilcmn <kilcmn3@hotmail.com>
2021-03-17 19:42:51 -0400

    sucessfully fetch submitted cards

---
+++ src/SubmittedCards.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const SubmittedCards = (props) => {
+  console.log(props);
+  return <div>submittedCards</div>;
+};
+
+export default SubmittedCards;
--- src/SubmittedContainer.jsx
+++ src/SubmittedContainer.jsx
@@ -1,11 +1,36 @@
 import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import SubmittedCard from './SubmittedCards';
 
 const SubmittedContainer = (props) => {
+  const [user, setUser] = useState(null);
+  const params = new useParams();
+
+  const fetchUserSubmition = () => {
+    return fetch(
+      `https://hacker-news.firebaseio.com/v0/user/${params.userName}.json?print=pretty`
+    )
+      .then((response) => response.json())
+      .then((data) => setUser(data));
+  };
+
   useEffect(() => {
+    fetchUserSubmition();
     // eslint-disable-next-line react-hooks/exhaustive-deps
+    return () => setUser(null);
   }, []);
 
-  return <div>SubmittedContainer</div>;
+  const renderSubmissionCards = (submissions) => {
+    return submissions.map((submission, index) => {
+      return <SubmittedCard key={index} submission={submission} />;
+    });
+  };
+  return (
+    <div>
+      SubmittedContainer
+      {user !== null ? renderSubmissionCards(user.submitted) : <div></div>}
+    </div>
+  );
 };
 
 export default SubmittedContainer;
