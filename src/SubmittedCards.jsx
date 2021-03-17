ebfa987a0a834becb660551371070f63874475de
kilcmn <kilcmn3@hotmail.com>
2021-03-17 19:46:41 -0400

    working on displaying user's submission at submisson container

--- src/SubmittedCards.jsx
+++ src/SubmittedCards.jsx
@@ -1,7 +1,22 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const SubmittedCards = (props) => {
-  console.log(props);
+  const [article, setarticles] = useState(null);
+
+  const fetchSubmssions = () => {
+    return fetch(
+      `https://hacker-news.firebaseio.com/v0/item/${props.submission}.json?print=pretty`
+    )
+      .then((response) => response.json())
+      .then((data) => setarticles(data));
+  };
+
+  useEffect(() => {
+    fetchSubmssions();
+    return () => setarticles(null);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+  console.log(article);
   return <div>submittedCards</div>;
 };
 
